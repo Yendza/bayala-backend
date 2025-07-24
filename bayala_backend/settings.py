cae04c0aa159b6225c92bb7b9105ce51a6d141cf
Yendza <yendzatech@gmail.com>
2025-07-24 12:51:25 +0200

    Corrige CORS para aceitar domínio do frontend no Render

--- bayala_backend/settings.py
+++ bayala_backend/settings.py
@@ -174,5 +174,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # frontend local
     "http://127.0.0.1:3000",
-    'http://bayalastock.vercel.app',
+    "https://bayalastock.vercel.app",
 ]
