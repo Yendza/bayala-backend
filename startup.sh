9624a064fe50260103de0f215cab40dde8b92787
Yendza <yendzatech@gmail.com>
2025-08-12 14:19:25 +0200

    Permissão de execução no startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Ativando ambiente virtual..."
-source ./venv/bin/activate  # ou ./.venv/bin/activate conforme o nome da sua pasta
+source ./venv/bin/activate  # ajuste se seu virtualenv estiver em outro lugar
 
 echo "Instalando dependências..."
 pip install -r requirements.txt
@@ -9,12 +9,15 @@ pip install -r requirements.txt
 echo "Aplicando migrações..."
 python manage.py migrate --noinput
 
+echo "Coletando arquivos estáticos..."
+python manage.py collectstatic --noinput
+
 echo "Criando superusuário..."
 python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username="Samuel").exists():
-    User.objects.create_superuser("Samuel", "bayala@bayala.com", "Bayala2025")
+if not User.objects.filter(username='Samuel').exists():
+    User.objects.create_superuser('Samuel', 'bayala@bayala.com', 'Bayala2025')
 END
 
 echo "Iniciando servidor..."
