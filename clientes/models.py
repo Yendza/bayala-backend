c82067ba70a77df6d61057405dd256957d28da69
Yendza <yendzatech@gmail.com>
2025-12-23 03:05:29 -1200

    Update models.py

--- clientes/models.py
+++ clientes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from core.models import Empresa  # importa o modelo Empresa
 
 class Cliente(models.Model):
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name="clientes")
     nome = models.CharField(max_length=255)
     nuit = models.CharField(max_length=50, blank=True, null=True)
     celular = models.CharField(max_length=50, blank=True, null=True)
