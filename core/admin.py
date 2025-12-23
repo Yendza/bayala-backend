ae7c7d2a6b755afd5ed925d3a1b3421338344d7a
Yendza <yendzatech@gmail.com>
2025-12-23 03:15:55 -1200

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from .models import Empresa
 
 admin.site.site_header = "Administração da CONTROLLER"
 admin.site.site_title = "Administração da CONTROLLER"
 admin.site.index_title = "Painel de Gestão"
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "nuit", "telefone", "email")
+    search_fields = ("nome", "nuit")
+
 
