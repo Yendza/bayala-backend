d710627cb17a198aaa9a7b51964cdacbcd54e16f
Yendza <yendzatech@gmail.com>
2025-12-23 02:54:09 -1200

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-admin.site.site_header = "Administração do BayalaStock"
-admin.site.site_title = "Administração do BayalaStock"
+admin.site.site_header = "Administração da CONTROLLER"
+admin.site.site_title = "Administração da CONTROLLER"
 admin.site.index_title = "Painel de Gestão"
 
