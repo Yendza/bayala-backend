ff5601b89e030113f97634e49af4035f34ef4f81
Yendza <yendzatech@gmail.com>
2025-12-23 03:02:32 -1200

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,4 +1,13 @@
-# Pode deixar vazio por agora, ou criar algo básico
 from django.db import models
 
-# Nenhum modelo por enquanto
+class Empresa(models.Model):
+    nome = models.CharField(max_length=200)
+    endereco = models.TextField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    telefone = models.CharField(max_length=50, blank=True, null=True)
+    cnpj = models.CharField(max_length=20, blank=True, null=True)  # ou NIF
+    ativo = models.BooleanField(default=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
